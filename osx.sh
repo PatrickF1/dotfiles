921d4a2dbff5c4836f430b94966da96810d111d8
Patrick Fong <pafong@groupon.com>
2015-05-12 21:59:59 -0700

    add disabling crash dialogue and showing path bar to osx config

--- osx.sh
+++ osx.sh
@@ -2,7 +2,7 @@
 # sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Set a blazingly fast key repeat rate
 defaults write -g InitialKeyRepeat -int 12
@@ -12,14 +12,21 @@ defaults write -g KeyRepeat -int 1
 defaults write com.apple.screencapture disable-shadow -bool true
 
 # Use list view in all Finder windows by default
-# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+# codes for the other view modes: `icnv`, `clmv`, `Flwv`
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
 # Show indicator lights for open applications in the Dock
 defaults write com.apple.dock show-process-indicators -bool true
 
+# Show the Finder path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
 # Expand the save and print panels by default
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
 defaults write -g PMPrintingExpandedStateForPrint -bool true
+
+# Disable the crash reporter
+defaults write com.apple.CrashReporter DialogType -string "none"
+
 
 echo "Done. Note that these changes may require a logout or restart to take effect."
