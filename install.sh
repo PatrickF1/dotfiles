25c8903dbf078fb25cef251a6a0335d8c890eaa3
Patrick Fong <patrickf3139@gmail.com>
2018-02-20 10:57:13 -0800

    backup more ST3 settings, move them into their own folder, and update installer

--- install.sh
+++ install.sh
@@ -6,16 +6,18 @@ REPO_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 for dotfile in $REPO_DIR/.{aliases,bash_profile,gitconfig,gitignore_global,functions}; do
     cp -i $dotfile ~
 done
-# install latest git-completion Bash script
+
+echo "Downloading the latest git-completion Bash script."
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 
-echo "Done. You must restart terminal or source the dot files before the changes to take effect."
+echo "Done. You must restart terminal or source the dot files before the changes will take effect."
 
 echo "Iteractively copying Sublime Text 3 settings into their home."
 
 SUBLIME_PREFERENCES_HOME="$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
-cp -i $REPO_DIR/Settings/Preferences.sublime-settings "$SUBLIME_PREFERENCES_HOME"
-cp -i "$REPO_DIR/Settings/Default (OSX).sublime-keymap" "$SUBLIME_PREFERENCES_HOME"
+for sublimeSettingsFile in "$REPO_DIR/Settings/Sublime Text 3/"*.sublime-settings; do
+    cp -i "$sublimeSettingsFile" "$SUBLIME_PREFERENCES_HOME"
+done
 echo "Done."
 
 sh $REPO_DIR/osx.sh
