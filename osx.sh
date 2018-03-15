9767fe258bbdc9aca395fd840b8a51b36bfbb986
Patrick Fong <patrickf3139@gmail.com>
2018-03-15 14:30:59 -0700

    add new settings to osx.sh

--- osx.sh
+++ osx.sh
@@ -1,13 +1,4 @@
 echo "Configuring OS X."
-# Ask for the administrator password upfront
-# sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
-# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-# Set a blazingly fast key repeat rate
-defaults write -g InitialKeyRepeat -int 12
-defaults write -g KeyRepeat -int 1
 
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
@@ -22,15 +13,27 @@ defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 # Show indicator lights for open applications in the Dock
 defaults write com.apple.dock show-process-indicators -bool true
 
+# Make Dock icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
+
 # Show the Finder path bar
 defaults write com.apple.finder ShowPathbar -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
 # Expand the save and print panels by default
 defaults write -g NSNavPanelExpandedStateForSaveMode -bool true
 defaults write -g PMPrintingExpandedStateForPrint -bool true
 
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
 # Disable the crash reporter
 defaults write com.apple.CrashReporter DialogType -string "none"
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 # Disable automatic emoji substitution (i.e. use plain text smileys)
 defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "automaticEmojiSubstitutionEnablediMessage" -bool false
