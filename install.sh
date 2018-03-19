5145d24079c164e7f242e48c36c81c1f99addf24
Patrick Fong <patrickf3139@gmail.com>
2018-03-19 11:19:00 -0700

    fix Sublime Text keymap not being copied on install

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 echo "Installing files for current user."
 echo "Interactively copying the dot files into the home directory."
 
@@ -15,7 +16,7 @@ echo "Done. You must restart terminal or source the dot files before the changes
 echo "Iteractively copying Sublime Text 3 settings into their home."
 
 SUBLIME_PREFERENCES_HOME="$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
-for sublimeSettingsFile in "$REPO_DIR/Settings/Sublime Text 3/"*.sublime-settings; do
+for sublimeSettingsFile in "$REPO_DIR/Settings/Sublime Text 3/"*; do
     cp -i "$sublimeSettingsFile" "$SUBLIME_PREFERENCES_HOME"
 done
 echo "Done."
