f09eccc11100ec74943ec2d3fde2706c10f9bb14
Patrick Fong <patrickf3139@gmail.com>
2018-03-26 16:08:16 -0700

    symlink rather than copy files over to their homes to keep this repo in sync more easily

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 echo "Installing files for current user."
-echo "Interactively copying the dot files into the home directory."
+echo "Interactively symlinking the dot files into the home directory."
 
 REPO_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 for dotfile in $REPO_DIR/.{aliases,bash_profile,gitconfig,gitignore_global,functions}; do
-    cp -i $dotfile ~
+    ln -i -s $dotfile ~
 done
 
 echo "Downloading the latest git-completion Bash script."
@@ -13,11 +13,11 @@ curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-com
 
 echo "Done. You must restart terminal or source the dot files before the changes will take effect."
 
-echo "Iteractively copying Sublime Text 3 settings into their home."
+echo "Iteractively symlinking Sublime Text 3 settings into their home."
 
 SUBLIME_PREFERENCES_HOME="$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
 for sublimeSettingsFile in "$REPO_DIR/Settings/Sublime Text 3/"*; do
-    cp -i "$sublimeSettingsFile" "$SUBLIME_PREFERENCES_HOME"
+    ln -i -s "$sublimeSettingsFile" "$SUBLIME_PREFERENCES_HOME"
 done
 echo "Done."
 
