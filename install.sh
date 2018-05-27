fd545612bb1346e9458bdfcd415d766f886333cc
Patrick Fong <patrickf3139@gmail.com>
2018-05-27 11:55:22 -0700

    add initial configurations for fish shell

--- install.sh
+++ install.sh
@@ -11,14 +11,17 @@ done
 echo "Downloading the latest git-completion Bash script."
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 
-echo "Done. You must restart terminal or source the dot files before the changes will take effect."
-
 echo "Iteractively symlinking Sublime Text 3 settings into their home."
-
 SUBLIME_PREFERENCES_HOME="$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
 for sublimeSettingsFile in "$REPO_DIR/Settings/Sublime Text 3/"*; do
     ln -i -s "$sublimeSettingsFile" "$SUBLIME_PREFERENCES_HOME"
 done
-echo "Done."
+
+echo "Interactively symlinking Fish config into its home."
+FISH_CONFIG_HOME="$HOME/.config/fish"
+mkdir -p $FISH_CONFIG_HOME
+ln -i -s "$REPO_DIR/Settings/config.fish" $FISH_CONFIG_HOME
 
 sh $REPO_DIR/osx.sh
+echo "Done. You must restart terminal or source the dot files before the changes will take effect."
+
