bf3f171d3f048e5468409ce361e97666b07a4fd6
Patrick Fong <patrickf3139@gmail.com>
2018-05-28 20:19:52 -0700

    fix config.fish being a symlink; make sure fish global variables are made environment variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,14 @@
-/Users/patrick/Code/dotfiles/Settings/config.fish
+fish_vi_key_bindings
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+status --is-interactive; and source (rbenv init -|psub)
+
+if test -e ~/.fish_secrets
+    source ~/.fish_secrets
+end
+
+# aliases
+alias clearf="pbpaste | pbcopy"
+
+set -xg EDITOR "/usr/local/bin/subl"
+set -xg theme_nerd_fonts yes
+set -xg theme_color_scheme base16
