303cc101e3b82413247a9e557cb1916fa10d23fe
Patrick Fong <patrickf3139@gmail.com>
2018-07-04 00:46:01 -0700

    optimize fzf fish integrations by using ripgrep and bfs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,12 +6,19 @@ if test -e ~/.fish_secrets
     source ~/.fish_secrets
 end
 
+set -xg VISUAL "vim"
+set -xg EDITOR "vim"
+
 # aliases
 alias clearf="pbpaste | pbcopy"
 
-set -xg VISUAL "vim"
-set -xg EDITOR "vim"
+# visual settings
 set -g theme_nerd_fonts yes
 set -g theme_color_scheme dark
-set -g FZF_LEGACY_KEYBINDINGS 0
 set -g fish_color_command B7D847
+
+# fzf plugin settings
+set -g FZF_LEGACY_KEYBINDINGS 0
+set -g FZF_FIND_FILE_COMMAND 'rg --files --hidden --follow --no-ignore-vcs -g !.git -g !node_modules -g !.idea 2> /dev/null'
+set -g FZF_CD_COMMAND bfs -type d -nohidden | sed s/^\./~/
+set -g FZF_CD_WITH_HIDDEN_COMMAND bfs -type d | sed s/^\./~/
