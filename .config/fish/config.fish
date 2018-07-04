f00b4e7e9486217e04bf321916e09081ab1acb13
Patrick Fong <patrickf3139@gmail.com>
2018-07-03 22:16:29 -0700

    for iTerm2, use material theme and customize fish color settings to make everything work well together

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,5 +12,6 @@ alias clearf="pbpaste | pbcopy"
 set -xg VISUAL "vim"
 set -xg EDITOR "vim"
 set -g theme_nerd_fonts yes
-set -g theme_color_scheme base16
-set -U FZF_LEGACY_KEYBINDINGS 0
+set -g theme_color_scheme dark
+set -g FZF_LEGACY_KEYBINDINGS 0
+set -g fish_color_command B7D847
