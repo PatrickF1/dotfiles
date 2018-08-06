6f44d0cf5fc35c007676a93d0ef3f1d30f745706
Patrick Fong <patrickf3139@gmail.com>
2018-08-05 20:17:00 -0700

    try differentiating tab colors again but modify colors to make them more distinguishable

--- fish/functions/__differentiate_tab_colors.fish
+++ fish/functions/__differentiate_tab_colors.fish
@@ -1,22 +1,17 @@
 #!/usr/local/bin/fish
 function __differentiate_tab_colors -d "set the current iTerm2 tab to the next color in a sequence of colors"
-    # these colors values are from the material-design iTerm theme
-    # https://github.com/MartinSeeler/iterm2-material-design
-    set black 546E7A
-    set red FF3D49
-    set green 69F0AE
-    set yellow FFD740
-    set blue 40C4FF
-    set magenta FF4081
-    set cyan 64FCDA
+    set red ff0d00
+    set green 1f8705
+    set yellow ffee03
+    set blue 034fff
     set white FFFFFF
-    set colors_array $black $red $green $yellow $blue $magenta $cyan $white
+    set colors_array $red $green $yellow $blue $white
 
     if set -q __tab_color_index
         # increment __tab_color_index so that the next tab will have a different color
         set __tab_color_index (math "$__tab_color_index + 1")
         # modulo so it doesn't keep growing
-        set __tab_color_index (math "$__tab_color_index % 8")
+        set __tab_color_index (math "$__tab_color_index % 5")
     else
         set -U __tab_color_index 0
     end
