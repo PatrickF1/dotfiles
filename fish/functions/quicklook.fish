a8c28629caadc89f483f430970e5ef5e1e8f120a
Patrick Fong <patrickf3139@gmail.com>
2018-08-09 11:47:57 -0700

    clarify call to qlmanage in quicklook.fish

--- fish/functions/quicklook.fish
+++ fish/functions/quicklook.fish
@@ -2,7 +2,8 @@
 # original from https://github.com/oh-my-fish/plugin-osx/blob/master/functions/ql.fish
 function quicklook -d "Quick Look a specified file or directory"
   if [ (count $argv) -gt 0 ]
-    qlmanage >/dev/null ^/dev/null -p $argv &
+    # start qlmanage in the background and redirect its stdout and stderr to /dev/null
+    qlmanage -p $argv >/dev/null ^&1 &
   else
     echo "No arguments given"
     return 1
