d56dc3118920bd278c2fba436b36b417d554b062
Patrick Fong <patrickf3139@gmail.com>
2019-02-14 15:35:41 -0800

    use fish_indent to properly indent all fish files

--- fish/functions/quicklook.fish
+++ fish/functions/quicklook.fish
@@ -1,11 +1,11 @@
 #!/usr/bin/env fish
 # original from https://github.com/oh-my-fish/plugin-osx/blob/master/functions/ql.fish
 function quicklook -d "Quick Look a specified file or directory"
-  if [ (count $argv) -gt 0 ]
-    # start qlmanage in the background and redirect its stdout and stderr to /dev/null
-    qlmanage -p $argv >/dev/null ^&1 &
-  else
-    echo "No arguments given"
-    return 1
-  end
+    if [ (count $argv) -gt 0 ]
+        # start qlmanage in the background and redirect its stdout and stderr to /dev/null
+        qlmanage -p $argv >/dev/null ^&1 &
+    else
+        echo "No arguments given"
+        return 1
+    end
 end
