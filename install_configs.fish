2029d33edad1d7a2f76845318d338bace72791d9
Patrick Fong <patrickf3139@gmail.com>
2019-07-30 12:08:49 -0700

    remove unnecessary line in install_vim_files

--- install_configs.fish
+++ install_configs.fish
@@ -79,7 +79,6 @@ function install_vim_files
     if not test -f ~/.local/share/nvim/site/autoload/plug.vim
         curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     end
-    ~/.local/share/nvim/site/autoload/plug.vim
 
     set -l vimrc "$repo_root/vim/.vimrc"
     ln -i -s $vimrc ~/.vimrc
