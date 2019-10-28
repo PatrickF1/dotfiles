566c5a308372c00f9a544cdf28019c95cd5c7ceb
Patrick Fong <patrickf3139@gmail.com>
2019-10-28 15:56:39 -0700

    update function name

--- .config/fish/functions/__reverse_i_search.fish
+++ .config/fish/functions/__reverse_i_search.fish
@@ -1,4 +1,4 @@
-function __reverse_i_search -d "Use fzf to reverse-i-serach"
+function __fzf_replace_command -d "Replace your commandline with a command found through fzf"
     history merge
     history -z | fzf --read0 --tiebreak=index --height 40% -q '(commandline)' | perl -pe 'chomp if eof' | read -lz result
     and commandline -- $result
