858d51fb498b3dc55158a3783d9c07a4a43bf18d
Patrick Fong <patrickf3139@gmail.com>
2019-10-31 23:09:00 -0700

    clean up bash configs a little

--- .config/bash/aliases.bash
+++ .config/bash/aliases.bash
@@ -8,13 +8,6 @@ alias vim="nvim"
 
 alias clearf="pbpaste | pbcopy"
 
-# Recursively delete `.DS_Store` files
-# alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
-
-# Show/hide hidden files in Finder
-alias showhidden="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
-alias hidehidden="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
-
 # Merge PDF files
 # Usage: `mergepdf -o output.pdf input{1,2,3}.pdf`
 alias mergepdf='/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py'
