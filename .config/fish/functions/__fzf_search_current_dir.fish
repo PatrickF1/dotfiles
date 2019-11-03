41f694e7a220a05bb4f70c68547e71dd99831d13
Patrick Fong <patrickf3139@gmail.com>
2019-11-02 21:40:54 -0700

    improve documentation: function descriptor and long form arguments

--- .config/fish/functions/__fzf_search_current_dir.fish
+++ .config/fish/functions/__fzf_search_current_dir.fish
@@ -1,13 +1,13 @@
-function __fzf_search_current_dir --description "Search the current directory for files and directories using fzf and fd."
+function __fzf_search_current_dir --description "Search the current directory for files and directories using fzf and fd. Insert the selected search results into the commandline at the cursor."
     fd --hidden --follow --exclude=.git 2> /dev/null |
     fzf --multi |
-    while read -l s
+    while read --local s
         set results $results $s
     end
-    commandline -t ""
+    commandline --current-token ""
     for result in $results
-        commandline -it -- (string escape $result)
-        commandline -it -- " "
+        commandline --insert --current-token -- (string escape $result)
+        commandline --insert --current-token -- " "
     end
     commandline --function repaint
 end
--- .config/fish/functions/__fzf_search_history.fish
+++ .config/fish/functions/__fzf_search_history.fish
@@ -1,9 +1,9 @@
-function __fzf_search_history --description "Search command history using fzf."
+function __fzf_search_history --description "Search command history using fzf. Replace the current commandline with the selected command."
     history merge
     history --null |
         fzf --read0 --tiebreak=index --height 40% --query=(commandline) |
         perl -pe 'chomp if eof' |
-        read -lz result
+        read --local --null result
     and commandline --replace $result
     commandline --function repaint
 end
