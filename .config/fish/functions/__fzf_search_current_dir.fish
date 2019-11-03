34481bc94a1f2945dbc3997ba4b0e9c3d1149fc5
Patrick Fong <patrickf3139@gmail.com>
2019-11-03 15:37:55 -0800

    colorize __fzf_search_current_dir

--- .config/fish/functions/__fzf_search_current_dir.fish
+++ .config/fish/functions/__fzf_search_current_dir.fish
@@ -1,6 +1,6 @@
 function __fzf_search_current_dir --description "Search the current directory using fzf and fd. Insert the selected results into the commandline at the cursor."
-    fd --hidden --follow --exclude=.git 2> /dev/null |
-    fzf --multi | # allows selecting multiple results using TAB
+    fd --hidden --follow --color=always --exclude=.git 2> /dev/null |
+    fzf --multi --ansi | # multi allows selecting multiple results using TAB
     while read --local s
         set results $results $s
     end
