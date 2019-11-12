1002c3ea1a84b46b534643eb29638b57c20e78f7
Patrick Fong <patrickf3139@gmail.com>
2019-11-12 11:55:55 -0800

    split command sub onto two lines and use test 0

--- .config/fish/functions/__fzf_search_history.fish
+++ .config/fish/functions/__fzf_search_history.fish
@@ -1,6 +1,9 @@
 function __fzf_search_history --description "Search command history using fzf. Replace the current commandline with the selected command."
     history merge
-    set command_selected (history --null | fzf --read0 --tiebreak=index --height 40% --query=(commandline))
+    set command_selected (
+        history --null |
+        fzf --read0 --tiebreak=index --height 40% --query=(commandline)
+    )
 
     if test $status -eq 0
         commandline --replace $command_selected
