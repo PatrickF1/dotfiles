0234db717f09237cf329906b9e24f600d00db038
Patrick Fong <patrickf3139@gmail.com>
2019-11-12 11:55:55 -0800

    split command sub onto two lines and use test 0

--- .config/fish/functions/__fzf_search_current_dir.fish
+++ .config/fish/functions/__fzf_search_current_dir.fish
@@ -1,12 +1,12 @@
 function __fzf_search_current_dir --description "Search the current directory using fzf and fd. Insert the selected results into the commandline at the cursor."
     set files_selected (
         fd --hidden --follow --color=always --exclude=.git 2> /dev/null |
-        fzf --multi --ansi | # multi allows selecting multiple results using TAB
-        xargs
+        fzf --multi --ansi
     )
 
-    if test (count $files_selected) -gt 0 # need to use count because files_selected contains newline if fzf is canceled
-        commandline --insert "$files_selected " # doesn't string escape so won't work with weird filesnames
+    if test $status -eq 0
+        commandline --insert (echo $files_selected | xargs) # doesn't string escape so won't work with weird filesnames
+        commandline --insert " "
     end
 
     commandline --function repaint
