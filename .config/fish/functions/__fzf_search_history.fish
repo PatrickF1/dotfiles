4726e7705050a745909bef6b4eece9a958611e96
Patrick Fong <patrickf3139@gmail.com>
2019-11-12 11:47:43 -0800

    simplify test for if command substitution succeeded

--- .config/fish/functions/__fzf_search_history.fish
+++ .config/fish/functions/__fzf_search_history.fish
@@ -1,7 +1,8 @@
 function __fzf_search_history --description "Search command history using fzf. Replace the current commandline with the selected command."
     history merge
     set command_selected (history --null | fzf --read0 --tiebreak=index --height 40% --query=(commandline))
-    if not test -z $command_selected
+
+    if test $status -eq 0
         commandline --replace $command_selected
     end
 
