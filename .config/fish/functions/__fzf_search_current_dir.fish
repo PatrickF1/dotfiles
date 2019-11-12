ed9c5e943e23a2d374a77130e3c82015cacedac6
Patrick Fong <patrickf3139@gmail.com>
2019-11-12 11:59:53 -0800

    fix typo

--- .config/fish/functions/__fzf_search_current_dir.fish
+++ .config/fish/functions/__fzf_search_current_dir.fish
@@ -1,11 +1,11 @@
-function __fzf_search_current_dir --description "Search the current directory using fzf and fd. Insert the selected results into the commandline at the cursor."
+function __fzf_search_current_dir --description "Search the current directory using fzf and fd. Insert the selected filesnames into the commandline at the cursor."
     set files_selected (
         fd --hidden --follow --color=always --exclude=.git 2> /dev/null |
         fzf --multi --ansi
     )
 
     if test $status -eq 0
-        commandline --insert (echo $files_selected | xargs) # doesn't string escape so won't work with weird filesnames
+        commandline --insert (echo $files_selected | xargs) # doesn't string escape so won't work with weird filenames
         commandline --insert " "
     end
 
