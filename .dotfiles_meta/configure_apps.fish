9a2258853bf150be9d71d0ba217f2f3d9de2972d
Patrick Fong <patrickf3139@gmail.com>
2019-12-03 16:23:12 -0800

    update configure_apps.fish

--- .dotfiles_meta/configure_apps.fish
+++ .dotfiles_meta/configure_apps.fish
@@ -1,16 +1,10 @@
 #!/usr/bin/env fish
 
-# Point iterm2 to settings stored in Dropbox
-set repo_root (dirname (realpath (status --current-filename)))
+# Point iterm2, Dash, and Alfred to settings stored in Dropbox
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/Dropbox/Settings"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
-
-# Point Dash to use settings stored in Dropbox
 defaults write com.kapeli.dashdoc syncFolderPath -string "$HOME/Dropbox/Settings"
-
-# Point Alfred to use settings stored in Dropbox
 defaults write com.runningwithcrayons.Alfred-Preferences syncfolder -string "$HOME/Dropbox/Settings"
 
 # Disable checking if keyboard shortcuts are on in Mailplane
 defaults write com.mailplaneapp.Mailplane3 DisableCheckKeyboardShortcuts -bool YES
-
