3ff0b40cfacc20df9af3af409aaae4bd2e4fb07d
Patrick Fong <patrickf3139@gmail.com>
2019-12-05 13:26:13 -0800

    configure Postico in configure_apps script

--- .dotfiles_meta/configure_apps.fish
+++ .dotfiles_meta/configure_apps.fish
@@ -7,4 +7,11 @@ defaults write com.kapeli.dashdoc syncFolderPath -string "$HOME/Dropbox/Settings
 defaults write com.runningwithcrayons.Alfred-Preferences syncfolder -string "$HOME/Dropbox/Settings"
 
 # Disable checking if keyboard shortcuts are on in Mailplane
-defaults write com.mailplaneapp.Mailplane3 DisableCheckKeyboardShortcuts -bool YES
+defaults write com.mailplaneapp.Mailplane3 DisableCheckKeyboardShortcuts -bool true
+
+# Configure Postico
+defaults write at.eggerapps.Postico AlternatingRows -bool true
+defaults write at.eggerapps.Postico IndentWithSpaces -bool true
+defaults write at.eggerapps.Postico QueryViewFontSize -int 14
+defaults write at.eggerapps.Postico TableViewFontSize -int 14
+defaults write at.eggerapps.Postico TableViewRowsPerPage -int 200
