24c2295f022f123503d8464d291508ca0e482633
Patrick Fong <patrickf3139@gmail.com>
2020-03-16 19:19:45 -0700

    alias python to python3

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,4 +1,4 @@
-function ls --description 'ls with improved defaults'
+function python --description 'ls with improved defaults'
     # -G Enable colorized output
     # -T When used with the -l option, display complete time information for the file
     # -h When used with the -l option, use unit suffixes: Byte, Kilobyte etc.
---
+++ .config/fish/functions/python.fish
@@ -0,0 +1,3 @@
+function python --description 'Alias python to python3'
+    command python3 $argv
+end
