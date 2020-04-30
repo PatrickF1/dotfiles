25f289054bd43a77b27fe341254337f96283910e
Patrick Fong <patrickf3139@gmail.com>
2020-04-30 09:20:20 -0700

    add asdf plugins automatically in install_shell_utilities.fish

--- .dotfiles_meta/install_shell_utilities.fish
+++ .dotfiles_meta/install_shell_utilities.fish
@@ -10,11 +10,15 @@ end
 echo "Installing vim-plug for neovim."
 curl --create-dirs --location --output ~/.local/share/nvim/site/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "Installing the latest git-completion script for bash."
+echo "Installing the latest git-completion script for bash"
 curl --create-dirs --location --output ~/.config/bash/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-echo "Installing the latest docker completions for fish."
+echo "Installing the latest docker completions for fish"
 curl --create-dirs --location --output ~/.config/fish/completions/docker.fish https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish
 
-echo "Installing fisher and fisher plugins."
+echo "Installing fisher and fisher plugins"
 curl --create-dirs --location --output ~/.config/fish/functions/fisher.fish https://git.io/fisher
 fish -c fisher
+
+echo "Installing plugins for asdf"
+asdf plugin add python
+asdf plugin add ruby
