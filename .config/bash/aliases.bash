8edb82001ae435810c1e198a879b58a618ea1967
Patrick Fong <patrickf3139@gmail.com>
2020-04-30 09:01:34 -0700

    clean up aliases

--- .config/bash/aliases.bash
+++ .config/bash/aliases.bash
@@ -5,9 +5,4 @@ alias grep="grep --color=auto"
 alias history="history | tail -r | less"
 alias ..="cd .."
 alias vim="nvim"
-
 alias clearf="pbpaste | pbcopy"
-
-# Merge PDF files
-# Usage: `mergepdf -o output.pdf input{1,2,3}.pdf`
-alias mergepdf='/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py'
