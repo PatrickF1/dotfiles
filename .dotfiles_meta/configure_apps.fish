03bd6e5f44b2bd70cf1e1ce9adfc092a8bc98362
Patrick <patrickf3139@gmail.com>
2020-05-08 22:55:31 -0700

    Set up Visual Studio Code (#14)

--- .dotfiles_meta/configure_apps.fish
+++ .dotfiles_meta/configure_apps.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 
+echo "Configuring GUI apps"
 # Point iterm2, Dash, and Alfred to settings stored in Dropbox
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$HOME/Dropbox/Settings"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
@@ -15,3 +16,6 @@ defaults write at.eggerapps.Postico IndentWithSpaces -bool true
 defaults write at.eggerapps.Postico QueryViewFontSize -int 14
 defaults write at.eggerapps.Postico TableViewFontSize -int 14
 defaults write at.eggerapps.Postico TableViewRowsPerPage -int 200
+
+echo "Installing VSCode extensions"
+cat vsc_extensions.txt | xargs -L 1 code --install-extension
