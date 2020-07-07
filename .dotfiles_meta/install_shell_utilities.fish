32366681ec8df1f440ba4fa1c5440a6219d11a8e
Patrick Fong <patrickf3139@gmail.com>
2020-07-07 12:05:16 -0700

    remove Docker fish completions because not using Docker anymore

--- .dotfiles_meta/install_shell_utilities.fish
+++ .dotfiles_meta/install_shell_utilities.fish
@@ -3,7 +3,7 @@
 set -l path_to_shell (which fish)
 if not grep -q "$path_to_shell" /etc/shells
     echo "Setting default shell to $path_to_shell"
-    echo "$path_to_shell" | sudo tee -a /etc/shells > /dev/null
+    echo "$path_to_shell" | sudo tee -a /etc/shells >/dev/null
     chsh -s "$path_to_shell"
 end
 
@@ -12,8 +12,6 @@ curl --create-dirs --location --output ~/.local/share/nvim/site/autoload/plug.vi
 
 echo "Installing the latest git-completion script for bash"
 curl --create-dirs --location --output ~/.config/bash/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-echo "Installing the latest docker completions for fish"
-curl --create-dirs --location --output ~/.config/fish/completions/docker.fish https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish
 
 echo "Installing fisher and fisher plugins"
 curl --create-dirs --location --output ~/.config/fish/functions/fisher.fish https://git.io/fisher
