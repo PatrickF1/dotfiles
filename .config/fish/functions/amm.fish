b1e9b793a2b77f59bc476a2888ec6708e15c2a79
Patrick Fong <patrickf3139@gmail.com>
2020-07-18 15:17:52 -0700

    Remove hack for making Ammonite work in Fish
    A patch has been made to Ammonite's upstream. Tested the latest Ammonite and it works without this hack.

--- .config/fish/functions/amm.fish
+++
@@ -1,5 +0,0 @@
-# work around for Ammonite not executing in the fish shell
-# https://github.com/lihaoyi/Ammonite/issues/813
-function amm --description 'Scala REPL'
-    sh -c 'amm "$@"' amm $argv
-end
