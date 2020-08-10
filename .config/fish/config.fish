1c593626bcd63bbb6d59397128b0c459e204ecd4
Patrick Fong <patrickf3139@gmail.com>
2020-08-09 21:32:38 -0700

    Use my new plugin for differentiating iterm tab colors
    Removing the code for differentiating iterm tab colors and switching to an external fish plugin to handle it. I extracted all of the logic into its own into fish package (https://github.com/patrickf3139/iterm_tab_colors.fish) to keep complex logic out of my dotfiles, which should only be focused on saving and setting up configuration. Now it is much easier to turn on and off by using fisher. Plus, the logic is now widely discoverable and portable.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,6 +22,5 @@ set -g theme_nerd_fonts yes
 set -g theme_color_scheme light
 set -g fish_color_command B7D847
 set -g theme_display_git_stashed_verbose yes
-# __differentiate_tab_colors
 
 source (brew --prefix asdf)/asdf.fish
