c87a5b214d791a19d9d2c588ebe25f5b912ecc3d
Patrick Fong <patrickf3139@gmail.com>
2020-08-20 14:29:32 -0700

    make dot inherit completions from git

--- .config/fish/functions/dot.fish
+++ .config/fish/functions/dot.fish
@@ -1,3 +1,3 @@
-function dot --description 'Manage dotfiles repository with home as working directory'
+function dot --wraps=git --description 'Manage dotfiles repository with home as working directory'
     git --git-dir="$HOME/Code/dotfiles/.git" --work-tree="$HOME" $argv
 end
