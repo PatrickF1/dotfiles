b35bed19b4243c1cd9ebbc8a2bbfd47eb4d16082
Patrick Fong <patrickf3139@gmail.com>
2022-09-15 17:07:44 -0700

    optimize n.fish and incorporate upstream changes from quitcd.fish

--- .config/fish/functions/n.fish
+++ .config/fish/functions/n.fish
@@ -1,21 +1,17 @@
 # source: https://github.com/jarun/nnn/blob/master/misc/quitcd/quitcd.fish
 function n --wraps nnn --description 'support nnn cd on quit'
     # Block nesting of nnn in subshells
-    if test -n "$NNNLVL"
-        if [ (expr $NNNLVL + 0) -ge 1 ]
-            echo "nnn is already running"
-            return
-        end
+    if test -n "$NNNLVL" -a "$NNNLVL" -ge 1
+        echo "nnn is already running"
+        return
     end
 
     # The behaviour is set to cd on quit (nnn checks if NNN_TMPFILE is set)
-    # To cd on quit only on ^G, remove the "-x" as in:
-    #    set NNN_TMPFILE "$XDG_CONFIG_HOME/nnn/.lastd"
-    #    (or, to a custom path: set NNN_TMPFILE "/tmp/.lastd")
-    # or, export NNN_TMPFILE after nnn invocation
+    # If NNN_TMPFILE is set to a custom path, it must be exported for nnn to
+    # see. To cd on quit only on ^G, remove the "-x" without changing the paths.
     set -x NNN_TMPFILE "$HOME/.config/nnn/.lastd"
 
-    nnn $argv
+    command nnn $argv
 
     if test -e $NNN_TMPFILE
         source $NNN_TMPFILE
