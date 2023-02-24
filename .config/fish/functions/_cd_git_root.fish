bddd50a548e739ac30896a44f1d6536873d2412c
Patrick Fong <patrickf3139@gmail.com>
2023-02-24 13:44:05 -0800

    fix _cd_git_root bug that cds to $HOME if not in a repo

--- .config/fish/functions/_cd_git_root.fish
+++ .config/fish/functions/_cd_git_root.fish
@@ -1,5 +1,12 @@
 # meant to be called interactively
 function _cd_git_root --description "Jump to the top-level directory of the current git repository."
-    cd (git rev-parse --show-toplevel)
+    set top_level_dir (git rev-parse --show-toplevel)
+    if test $status -eq 0
+        if test $top_level_dir != (pwd)
+            cd $top_level_dir
+        else
+            echo "Already in top level of git repo" >&2
+        end
+    end
     commandline --function repaint
 end
