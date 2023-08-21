71ca1887f5b4928cab7e906b9c6f131773fee510
Patrick Fong <patrickf3139@gmail.com>
2023-08-20 21:43:48 -0700

    integrate fzf.fish with delta, overwrite author date when amending
    fzf.fish highlighter integration implemented in https://github.com/PatrickF1/fzf.fish/commit/90a66b91421f9d1459f5dec92d2a06d01164def3

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -29,6 +29,7 @@ set -x fish_color_command B7D847
 set -g fish_greeting
 
 fzf_configure_bindings --directory=\cf --git_log=\cl --git_status=\cs --processes=\cp
+set fzf_diff_highlighter delta --paging=never
 
 set -x FZF_DEFAULT_OPTS --cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"
 
