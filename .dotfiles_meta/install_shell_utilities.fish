8baa439c5f5d77d1fdd5f78deaf6f3c25dcc2772
Patrick Fong <patrickf3139@gmail.com>
2023-10-07 23:30:21 -0700

    Tide: use a sparser, richer config to take advantage of transient prompt
    https://github.com/romkatv/powerlevel10k/blob/master/README.md#transient-prompt

--- .dotfiles_meta/install_shell_utilities.fish
+++ .dotfiles_meta/install_shell_utilities.fish
@@ -27,9 +27,8 @@ end
 
 if test (count $tide_right_prompt_items) -gt 10
     echo "Configuring Tide prompt"
-    tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time=No --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Few icons' --transient=Yes
-    # only show a few items on right side of prompt to keep it short and unintrusive
-    set tide_right_prompt_items status cmd_duration context
+    tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='12-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, character' --prompt_connection=Dotted --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Lightest --prompt_spacing=Sparse --icons='Many icons' --transient=Yes
+    set tide_right_prompt_items status cmd_duration time
     tide reload # reload makes the changes take effect
 end
 
