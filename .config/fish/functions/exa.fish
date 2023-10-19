7c12d83bfca83336e9a4053a2640e720bc073fac
Patrick Fong <patrickf3139@gmail.com>
2023-10-19 11:21:03 -0700

    replace exa with eza
    https://github.com/ogham/exa/issues/1243 is now unmaintained.
    
    eza is a direct descendent of exa

---
+++ .config/fish/functions/exa.fish
@@ -0,0 +1,4 @@
+# exa is easier to type and is what my muscle memory defaults to
+function exa --wraps eza --description "Alias exa to eza"
+    command eza $argv
+end
--- .config/fish/functions/v.fish
+++ .config/fish/functions/v.fish
@@ -1,4 +1,3 @@
-#!/usr/bin/env fish
 function v --wraps nvim --description "Alias v to nvim"
     command nvim $argv
 end
