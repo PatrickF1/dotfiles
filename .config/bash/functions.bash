519904fe75f52ac50b37614b299c7e498715e9fb
Patrick Fong <patrickf3139@gmail.com>
2025-03-04 00:28:12 -0800

    add y shell wrapper

--- .config/bash/functions.bash
+++ .config/bash/functions.bash
@@ -2,3 +2,12 @@
 function git-root() {
     cd "$(git rev-parse --show-toplevel)"
 }
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
