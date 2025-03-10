d8b7fece3fe47096fdae37736ceade9e21cf4f4d
Patrick Fong <patrickf3139@gmail.com>
2025-03-10 13:25:35 -0700

    Migrate from nnn to Yazi (#28)
    Migrating off of nnn because
    - confusing, inconsistent behavior I couldn't get used to, especially
    file navigation and filter(see my comments in
    https://github.com/jarun/nnn/issues/1219)
    - poor integration with fzf and z
    - hard to configure file previews
    - official plugins are haphazardly written and undocumented
    - I'd give overall docs a D
    
    Migrating to yazi
    - UX is very intuitive was easy for me to master
    - out of box integrations with fzf and z
    - previews work out of the box
    - very sane defaults and key bindings; I had to do almost no
    configuration
    - richer ecosystem of plugins that seem to be decently maintained
    - I'd give overall docs a C+ (slightly better)
    - on the downside, it is too powerful for my taste, and I doubt I will
    ever use half of the features

--- .config/bash/functions.bash
+++ .config/bash/functions.bash
@@ -3,32 +3,11 @@ function git-root() {
     cd "$(git rev-parse --show-toplevel)"
 }
 
-# source: https://github.com/jarun/nnn/blob/master/misc/quitcd/quitcd.bash_zsh
-n ()
-{
-    # Block nesting of nnn in subshells
-    if [ -n $NNNLVL ] && [ "${NNNLVL:-0}" -ge 1 ]; then
-        echo "nnn is already running"
-        return
-    fi
-
-    # The behaviour is set to cd on quit (nnn checks if NNN_TMPFILE is set)
-    # To cd on quit only on ^G, either remove the "export" as in:
-    #    NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
-    #    (or, to a custom path: NNN_TMPFILE=/tmp/.lastd)
-    # or, export NNN_TMPFILE after nnn invocation
-    export NNN_TMPFILE="${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.lastd"
-
-    # Unmask ^Q (, ^V etc.) (if required, see `stty -a`) to Quit nnn
-    # stty start undef
-    # stty stop undef
-    # stty lwrap undef
-    # stty lnext undef
-
-    nnn "$@"
-
-    if [ -f "$NNN_TMPFILE" ]; then
-        . "$NNN_TMPFILE"
-        rm -f "$NNN_TMPFILE" > /dev/null
-    fi
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
 }
